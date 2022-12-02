640498bdf2562d987dd44a2a4cdc1456c1dc9067
Chalez <charlesincollege7@gmail.com>
2022-12-01 21:18:02 -0500

    Add project files.

---
+++ AdventOfCode22/Day1/Day1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Transactions;
+
+namespace AdventOfCode22.Day1
+{
+    class Day1
+    {
+
+        public static string readFromFile()
+        {
+            string path = @"input";
+
+            return File.ReadAllText(path);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! This is Day 1");
+
+            string puzzleInput = readFromFile();
+
+            //Separate string input into an array of strings split by '\n'
+            string[] calories = puzzleInput.Split("\n");
+
+            int max = 0;
+            int sum = 0;
+
+
+            //for each string
+            foreach (var line in calories) {
+
+                //if it's a num
+                if (line != "")
+                {
+                    //Parse string into num, add into sum
+                    int num = int.Parse(line);
+                    sum += num;
+                }
+                else
+                {
+                    //if sum is greater than max
+                    if (sum > max)
+                    {
+                        //it is the new max
+                        max = sum;
+                    }
+                    //reset sum for next group of strings
+                    sum = 0;
+                }
+                
+
+            }
+
+            Console.WriteLine(max);
+
+
+        }
+
+
+    }
+}
---
+++ AdventOfCode22/Day1/Day1Part2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode22.Day1
+{
+    class Day1Part2
+    {
+
+        public static string readFromFile()
+        {
+            string path = @"input";
+
+            return File.ReadAllText(path);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! This is Day 1");
+
+            string puzzleInput = readFromFile();
+
+            //Separate string input into an array of strings split by '\n'
+            string[] calories = puzzleInput.Split("\n");
+
+            int max = 0;
+            int sum = 0;
+
+            List<int> values = new List<int>();
+
+            //for each string
+            foreach (var line in calories)
+            {
+
+                //if it's a num
+                if (line != "")
+                {
+                    //Parse string into num, add into sum
+                    int num = int.Parse(line);
+                    sum += num;
+                }
+                else
+                {
+                    values.Add(sum);
+
+                    //reset sum for next group of strings
+                    sum = 0;
+                }
+
+            }
+
+            values.Sort();
+
+            values.Reverse();
+
+            Console.WriteLine(values[0] + values[1] + values[2]);
+
+
+
+        }
+
+
+    }
+}
+
